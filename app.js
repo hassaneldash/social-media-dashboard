04cd4b4ce4f0650a9821bf8c890a0b35918cdf3f
hassaneldash <hassan.eldash@yahoo.com>
2024-06-18 02:07:06 +0300

    Modified Final

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const connectDB = require('./config');
 const bodyParser = require('body-parser');
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 connectDB();
 
 app.use(bodyParser.json());
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/posts', require('./routes/posts'));
